84a2f9a8f38ad16315fcbd6fddb1ffbc3452a187
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-10 09:22:39 +0530

    Second code

---
+++ five.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Please enter your user name"
+read USERNAME
+echo "Entred username is $USERNAME"
+echo "Please enter your password"
+read PASSWORD
+echo "Entred password is $PASSWORD"
