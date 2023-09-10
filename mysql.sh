b334190d3d42b0dc3fb3fb734387bc00ff449150
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-10 10:26:48 +0530

    git and mysql

--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 USER=$(id -u)
 if [ $USER -ne 0 ]
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 then 
-echo "ERROR:: Your not in root, Kindly get root access"
+echo "ERROR:: $R Your not in root, Kindly get root access $N"
     exit 1
 else
-    echo "Your in root...........Mysql start downloading"
+    echo " $G Your in root...........Mysql start downloading $N"
 fi
-yum install mysqlll -y
+yum install mysql -y
 if [ $? -ne 0 ]
 then
-    echo "Mysql was not downloaded there is some error kincldy check error and again start downloading"
+    echo "$R Mysql was not downloaded there is some error kincldy check error and again start downloading $N"
     exit 1
 else
-    echo "Mysql was downloaded successfully.........."
+    echo "$G Mysql was downloaded successfully..........$N"
+fi
+yum install postfix -y
+if [ $? -ne 0 ]
+then
+    echo " $R postfix was not downloaded there is some error kincldy check error and again start downloading $N"
+    exit 1
+else
+    echo " $G postfix was downloaded successfully..........$N"
 fi
