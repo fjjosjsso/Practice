7cb7fcf6b1322253c4947a511aada770f9fccb54
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-10 10:28:42 +0530

    ddddddddd

--- mysql.sh
+++ mysql.sh
@@ -5,24 +5,24 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 then 
-echo "ERROR:: $R Your not in root, Kindly get root access $N"
+echo -e "ERROR:: $R Your not in root, Kindly get root access $N"
     exit 1
 else
-    echo " $G Your in root...........Mysql start downloading $N"
+    echo -e " $G Your in root...........Mysql start downloading $N"
 fi
 yum install mysql -y
 if [ $? -ne 0 ]
 then
-    echo "$R Mysql was not downloaded there is some error kincldy check error and again start downloading $N"
+    echo -e "$R Mysql was not downloaded there is some error kincldy check error and again start downloading $N"
     exit 1
 else
-    echo "$G Mysql was downloaded successfully..........$N"
+    echo -e "$G Mysql was downloaded successfully..........$N"
 fi
 yum install postfix -y
 if [ $? -ne 0 ]
 then
-    echo " $R postfix was not downloaded there is some error kincldy check error and again start downloading $N"
+    echo -e " $R postfix was not downloaded there is some error kincldy check error and again start downloading $N"
     exit 1
 else
-    echo " $G postfix was downloaded successfully..........$N"
+    echo  -e " $G postfix was downloaded successfully..........$N"
 fi
