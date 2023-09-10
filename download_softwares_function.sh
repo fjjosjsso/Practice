6486d3c656d88797d7761b23c2cd8c5a6591c354
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-10 11:21:28 +0530

    all software downlaods

---
+++ download_softwares_function.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+USER=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+if [ $USER -ne 0 ]
+then
+    echo -e "$R YOUR NOT IN ROOT ACCESS $N"
+else
+    echo -e "$G YOUR IN ROOT ACCESS $N"
+fi
+vaildate (){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$R $2 FAILURE $N"
+    else
+    echo -e "$G $2 SUCCESSFULL $N"
+}
+yum install $i -y
+vaildate $? "$i  ........Installing"
