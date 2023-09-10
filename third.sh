9a885643b97288d9e386436e5f7251277446c40d
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-10 09:06:04 +0530

    Second code

---
+++ third.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$1 :: Hello $2. Good morning "
+echo "$2 :: Good morning $2 . How are you"
+echo "$1 :: I am good what about you "
+echo "$2 :: Thanks for asking, fuck off"
