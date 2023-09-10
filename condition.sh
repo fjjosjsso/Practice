d341326927207969cbd557b49c687a5880203411
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-10 09:45:28 +0530

    ddd

--- condition.sh
+++ condition.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 NUMBER=$1
+R="\e[31m"
+G="e[32"
+n="e[0"
+
 if [ $1 -ne 25 ]
 then
-echo "KINDLY CHECK YOUR TEXTE NUMBER ONCE"
+echo " $R KINDLY CHECK YOUR TEXTE NUMBER ONCE $N"
 else
-echo "ENTER NUBMER IS RIGHT "
+echo " $G ENTER NUBMER IS RIGHT $N "
 fi
