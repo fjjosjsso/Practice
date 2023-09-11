608706888057f96d733b5e5d58e7d02ba0d007e2
fjjosjsso <sakaraysairam1996@gmail.com>
2023-09-11 07:31:48 +0530

    GOOD

--- validation.sh
+++ validation.sh
@@ -3,18 +3,20 @@ USER=$(id -u)
 DATE=$(date +%F/%H/%M/%S)
 SCRIP_NAME=$0
 LOGFILES=/tmp/$SCRIP_NAME-$DATE.log
-if [ $SUER -ne 0 ]
+if [ $USER -ne 0 ]
 then
     echo " $R ERROR :: YOUR NOT IN ROOT ACCESS $N"
 else
     echo " $G YOUR IN ROOT ACCESS $N"
 fi
+vaildate(){
 if [ $1 -ne 0 ]
 then
     echo "$R PACKAGE INSTALLING .......IS FAILURE $N "
 else 
     echo "$G PACKAGE INSTALLING .........IS SUCCUSS $N"
 fi 
+}
 yum install git -y &>> LOGFILES
 vaildate $?
 yum install mysql-y &>> LOGFILES
